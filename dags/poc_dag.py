15c0f64b3ece305de18f1f47968e1a6557f76af3
Nikas <athnikas@CPX-7XRGR7X2Z58.dir.svc.accenture.com>
2022-11-17 11:40:42 +0200

    removing logging code

--- dags/poc_dag.py
+++ dags/poc_dag.py
@@ -2,14 +2,6 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime
-import logging
-import sys
-
-# Set Task Logger to INFO for better task logs
-log = logging.getLogger("airflow.task")
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-log.addHandler(handler)
 
 with DAG("dbt_dag", start_date=datetime(2021, 1, 1), schedule_interval=None, catchup=False) as dag:
 
